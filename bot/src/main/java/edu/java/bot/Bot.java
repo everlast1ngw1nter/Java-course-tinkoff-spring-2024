fd8b44b040f026b56f8fa7019b872cdf7332e051
everlast1ngw1nter <at07012004@gmail.com>
2024-02-17 15:25:12 +0500

    hw1

--- bot/src/main/java/edu/java/bot/Bot.java
+++ bot/src/main/java/edu/java/bot/Bot.java
@@ -1,15 +1,10 @@
 package edu.java.bot;
 
-import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import edu.java.bot.configuration.ApplicationConfig;
 import edu.java.bot.messageprocessor.ProcessorHolder;
-import java.io.IOException;
-import java.util.List;
 
 public class Bot {
 
@@ -21,28 +16,14 @@ public class Bot {
 
     public void start() {
         TelegramBotService.setMenuCommands(telegramBot);
-        telegramBot.setUpdatesListener(new UpdatesListener() {
-            @Override
-            public int process(List<Update> list) {
-                for (var elem : list) {
-
-                    var res = ProcessorHolder.PROCESSOR.process(elem);
-                    var id = elem.message().chat().id();
-                    SendMessage request = new SendMessage(id, res);
-                    telegramBot.execute(request, new Callback<SendMessage, SendResponse>() {
-                        @Override
-                        public void onResponse(SendMessage request, SendResponse response) {
-
-                        }
-
-                        @Override
-                        public void onFailure(SendMessage request, IOException e) {
-
-                        }
-                    });
-                }
-                return CONFIRMED_UPDATES_ALL;
+        telegramBot.setUpdatesListener(list -> {
+            for (var elem : list) {
+                var res = ProcessorHolder.PROCESSOR.process(elem);
+                var id = elem.message().chat().id();
+                SendMessage request = new SendMessage(id, res);
+                telegramBot.execute(request);
             }
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
     }
 }
