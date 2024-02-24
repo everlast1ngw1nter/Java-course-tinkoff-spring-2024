c65853c381674c146143ff1b8267c20904ccbe12
everlast1ngw1nter <at07012004@gmail.com>
2024-02-25 01:25:29 +0500

    hw2

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -9,6 +9,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ScrapperApplication.class, args);
+        var ctx = SpringApplication.run(ScrapperApplication.class, args);
+        var cli = (StackOverflowClient) ctx.getBean("stackOverflowClient");
+        var res = cli.someRestCall("76431324").toEntity(String.class).block();
+        System.out.println(res);
     }
 }
