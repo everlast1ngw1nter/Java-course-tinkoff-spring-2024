2b6e623f93dbd1f3541088bd2aac459baf67dbf6
everlast1ngw1nter <at07012004@gmail.com>
2024-03-17 16:52:32 +0500

    hw5

--- scrapper/src/main/java/edu/java/configuration/ServicesConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ServicesConfig.java
@@ -5,6 +5,7 @@ import edu.java.domain.services.jdbc.JdbcLinkUpdater;
 import edu.java.domain.services.jdbc.JdbcTgChatService;
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
+import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +17,20 @@ public class ServicesConfig {
 
     @Bean
     public static DataSource dataSource() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/scrapper");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("postgres");
+        return dataSource;
+        /*
         return DataSourceBuilder.create()
-                .url("jdbc:postgresql://localhost:5432/scrapper")
-                .username("postgres")
-                .password("postgres")
-                .build();
+        .url("jdbc:postgresql://localhost:5432/scrapper")
+        .username("postgres")
+        .password("postgres")
+        .build();
+        Factory method 'dataSource' threw exception with message: No supported DataSource type found
+        это почему-то работает в тестах, но не здесь, хотя зависимости вроде все проставлены
+        */
     }
 
     @Bean
